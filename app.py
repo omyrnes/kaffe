ebeb57566f96543a46d93519c1c1f991bd1b758c
omyrnes <82180649+omyrnes@users.noreply.github.com>
2025-11-07 20:28:20 +0100

    Init

---
+++ app.py
@@ -0,0 +1,29 @@
+from dash import Dash, dcc, html, Input, Output, callback
+
+app = Dash(__name__)
+app.title = "Kaffe"
+
+app.layout = html.Div([
+    html.Div(
+        children=html.Div([
+            html.H1('Kaffekalkulator')
+        ])
+    ),   
+    html.Div(["Oppgi vann i ml: ",
+        dcc.Input(id='my-input', value="initial-value", type='number')
+    ]),
+    html.Br(),
+    html.Div(id='my-output'),
+
+])
+
+@callback(
+    Output(component_id='my-output', component_property='children'),
+    Input(component_id='my-input', component_property='value')
+)
+def vanntilkaffe(vann, forhold = (1000/60)):
+    mengdekaffe = round(vann/forhold, 2)
+    return f'Gram kaffe: {mengdekaffe} g'
+
+if __name__ == '__main__':
+    app.run(debug=True)
